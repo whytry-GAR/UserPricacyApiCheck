f1284ba747214a41551a0c6dba81e1d44c26df5f
wuhonghao <gzwuhonghao@corp.netease.com>
2021-10-19 15:44:14 +0800

    增加getPackagesForUid和queryIntentActivities方法

--- app/src/main/java/com/wizard/userpricacyapicheck/hook/descripe/ApplicationPackageManagerHook.java
+++ app/src/main/java/com/wizard/userpricacyapicheck/hook/descripe/ApplicationPackageManagerHook.java
@@ -1,5 +1,6 @@
 package com.wizard.userpricacyapicheck.hook.descripe;
 
+import android.content.Intent;
 import com.wizard.userpricacyapicheck.hook.HookDescribe;
 import com.wizard.userpricacyapicheck.hook.model.HookInfo;
 import com.wizard.userpricacyapicheck.hook.model.MethodInfo;
@@ -19,6 +20,8 @@ public class ApplicationPackageManagerHook extends HookDescribe {
             .addMethodInfo(new MethodInfo("getInstalledApplications", "int"))
             .addMethodInfo(new MethodInfo("getInstalledPackages", "int"))
             .addMethodInfo(new MethodInfo("getInstalledModules", "int"))
+            .addMethodInfo(new MethodInfo("getPackagesForUid", "int"))
+            .addMethodInfo(new MethodInfo("queryIntentActivities", Intent.class.getName(), "int"))
         ;
         return hookInfo;
     }
